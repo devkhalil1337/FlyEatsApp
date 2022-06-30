37abb5c730aa60f855f8d763f2395243290b050e
Khalil Asif <58604863+devkhalil1337@users.noreply.github.com>
2022-07-01 00:18:09 +0500

    BusinessInfo Controller added

---
+++ FlyEatsApp/Controllers/BusinessInfoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FlyEatsApp.Models;
+using FlyEatsApp.Providers;
+
+namespace FlyEatsApp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BusinessInfoController : ControllerBase
+    {
+
+        [HttpPost]
+        public long AddNewBusinessUnit(BusinessInfo businessInfo)
+        {
+            BusinessInfoProvider businessInfoProvider = new BusinessInfoProvider();
+            var result = businessInfoProvider.AddNewBusinessUnit(businessInfo);
+            return result;
+        }
+
+       [HttpPut]
+        public Boolean UpdateBusinessUnit(BusinessInfo businessInfo)
+        {
+            BusinessInfoProvider businessInfoProvider = new BusinessInfoProvider();
+            var result = businessInfoProvider.UpdateBusinessUnit(businessInfo);
+            return result;
+        }
+
+        [HttpGet]
+       public IEnumerable<BusinessInfo> GetBusinessUnitById(int BusinessId)
+       {
+           BusinessInfoProvider businessInfoProvider = new BusinessInfoProvider();
+           var result = businessInfoProvider.GetBusinessUnitById(BusinessId);
+           return result;
+       }
+
+       [HttpDelete]
+       public Boolean DeleteBusinessUnit(long BusinessId)
+       {
+           BusinessInfoProvider businessInfoProvider = new BusinessInfoProvider();
+           var result = businessInfoProvider.DeleteBusinessUnit(BusinessId);
+           return result;
+       }
+    }
+}
