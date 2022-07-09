5d75e117ab9985b356bd56f047c4916d97cad357
Kamran <rkamranasif@gmail.com>
2022-07-10 02:24:45 +0500

    Product selection model updated

--- FlyEatsApp/Models/ProductSelection.cs
+++ FlyEatsApp/Models/ProductSelection.cs
@@ -13,6 +13,7 @@ namespace FlyEatsApp.Models
         public const string SELECTION_ID_COLUMN = "SelectionId";
         public const string PRODUCT_SELECTION_BUSINESS_ID_COLUMN = "BusinessId";
         public const string PRODUCT_SELECTION_CREATE_DATE_COLUMN = "CreationDate";
+        public const string PRODUCT_SELECTION_UPDATE_DATE_COLUMN = "UpdateDate";
    
 
         public int? ProductSelectionId { get; set; }
@@ -35,8 +36,20 @@ namespace FlyEatsApp.Models
                 }
             }
         }
+        public DateTime? ModifyDate { get; set; }
+        public string? UpdateDate
+        {
+            get { return ModifyDate.HasValue ? ModifyDate.Value.ToString("yyyy-MM-dd hh:mm:ss") : null; }
 
-       
+            set
+            {
+                if (value != null)
+                {
+                    ModifyDate = DateTime.ParseExact(value, "yyyy-MM-dd hh:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+                }
+            }
+        }
+
         public static ProductSelection ExtractObject(DataRow dataRow)
         {
             var newObject = new ProductSelection();
@@ -45,6 +58,7 @@ namespace FlyEatsApp.Models
             newObject.SelectionId = Convert.ToInt32(dataRow[SELECTION_ID_COLUMN]);
             newObject.BusinessId = Convert.ToInt32(dataRow[PRODUCT_SELECTION_BUSINESS_ID_COLUMN]);
             newObject.CreateDate = dataRow[PRODUCT_SELECTION_CREATE_DATE_COLUMN] == DBNull.Value ? null : (DateTime?)Convert.ToDateTime(dataRow[PRODUCT_SELECTION_CREATE_DATE_COLUMN]);
+            newObject.ModifyDate = dataRow[PRODUCT_SELECTION_UPDATE_DATE_COLUMN] == DBNull.Value ? null : (DateTime?)Convert.ToDateTime(dataRow[PRODUCT_SELECTION_UPDATE_DATE_COLUMN]);
            
             return newObject;
         }
