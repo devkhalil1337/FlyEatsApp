2fef6bacbb5a5098dcdb7e664743677ca3e3fe34
devkhalil1337 <devkhalil1337@gmail.com>
2023-02-11 17:01:40 +0500

    Variant added to order details SP script

--- DataAccessLayer/Scripts/SP_GetOrdersDetailsByOrderId.sql
+++ DataAccessLayer/Scripts/SP_GetOrdersDetailsByOrderId.sql
@@ -1,14 +1,14 @@
 USE [Flyeats]
 GO
-/****** Object:  StoredProcedure [dbo].[SP_GetAllOrdersByBusinessId]    Script Date: 2/9/2023 10:05:04 PM ******/
+/****** Object:  StoredProcedure [dbo].[SP_GetOrdersDetailsByOrderId]    Script Date: 2/11/2023 4:30:25 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-Create PROCEDURE [dbo].[SP_GetOrdersDetailsByOrderId]
+ALTER PROCEDURE [dbo].[SP_GetOrdersDetailsByOrderId]
 (
-	@OrderId int
+	@OrderId  nvarchar(255)
 )
 AS  
 BEGIN  
